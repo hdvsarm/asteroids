302485752d328cc09f7dc12cda3fd1b1fe8bd561
hdvsarm <harrison.arms@gmail.com>
2025-04-05 13:12:27 -0500

    added player rotation

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH, ASTEROID_KINDS, ASTEROID_MAX_RADIUS, ASTEROID_MIN_RADIUS, ASTEROID_SPAWN_RATE
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player=Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
     running=True
     while running:
         for event in pygame.event.get():
@@ -11,7 +15,11 @@ def main():
                 return
 
         screen.fill("black")  # Fill screen every frame
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()  # Update the display
+        delta = clock.tick(60)
+        dt=delta/1000
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
