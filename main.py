f09291e5d2c8c923c9cb2746d1ce3146997d84f1
hdvsarm <harrison.arms@gmail.com>
2025-04-05 13:35:36 -0500

    switched to using groups

--- main.py
+++ main.py
@@ -7,16 +7,19 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    player=Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player=Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     running=True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
+        updatable.update(dt)
         screen.fill("black")  # Fill screen every frame
-        player.draw(screen)
-        player.update(dt)
+        for obj in drawable:
+            obj.draw(screen)  
         pygame.display.flip()  # Update the display
         delta = clock.tick(60)
         dt=delta/1000
